c69409489fd8d8214ec186d68ea840c18a328011
danieTalmon <daniel2300ster@gmail.com>
2021-12-05 23:07:37 +0200

    init

--- whizzco-dashboard-app/src/app/app.module.ts
+++ whizzco-dashboard-app/src/app/app.module.ts
@@ -1,20 +1,14 @@
 import { SharedModule } from './shared/shared/shared.module';
- import { WebsitesReportModule } from './websites-report/websites-report.module';
+import { WebsiteReportsModule } from './websites-report/websites-report.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    WebsitesReportModule,
-    SharedModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, WebsiteReportsModule, SharedModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
